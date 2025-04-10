5ca253835881406f19acb4014b03491cb48c910f
Hugo Rivera <146773462+hugoriv18@users.noreply.github.com>
2025-04-09 21:45:14 -0600

    Add files via upload

---
+++ FirstGen/likelihood/PkMultipolesLikelihood.py
@@ -0,0 +1,80 @@
+import numpy as np
+from cobaya.likelihood import Likelihood
+from scipy.linalg import cho_factor, cho_solve
+from .get_model import compute_multipoles_model
+
+basepath = '/Users/hugor/OneDrive/Escritorio/NISDB/first_gen/data/'
+
+class PkMultipolesLikelihood(Likelihood):
+    """
+    Cobaya-compatible likelihood for power spectrum multipoles (P0, P2, P4).
+    Uses a fixed data vector and covariance matrix.
+    """
+    required_parameters = ["h", "omega_b", "omega_cdm", "ns", "log10As", "Mnu", "b1"]
+    # These will be passed from YAML
+    kmax: float
+    zeff: float
+
+    def initialize(self):
+        # Set fixed redshift
+        self.zeff = float(self.zeff)
+        # kmax mode
+        self.kmax = float(self.kmax)
+        # Fixed file paths to data
+        k_full = np.loadtxt(basepath + "k.txt")
+        Pk0_full = np.loadtxt(basepath + "Pk_0.txt")[:, 0]
+        Pk2_full = np.loadtxt(basepath + "Pk_2.txt")[:, 0]
+        Pk4_full = np.loadtxt(basepath + "Pk_4.txt")[:, 0]
+        cov_full = np.loadtxt(basepath + "cov.txt")
+        # Cut the k modes
+        mask = k_full <= self.kmax
+        self.k_arr = k_full[mask] # Shape: (N,)
+        # Cut the multipoles
+        Pk0 = Pk0_full[mask]
+        Pk2 = Pk2_full[mask]
+        Pk4 = Pk4_full[mask]
+        self.data_vector = np.concatenate([Pk0, Pk2, Pk4]) # Shape: (3N,)
+        # Cut the matrix
+        kcenters = k_full  # x-axis values used to index the covariance matrix
+        ellscov = (0, 2, 4)  # multipole orders in the full data vector
+        klim = {ell: (k_full[0], self.kmax, 0.005) for ell in ellscov}
+        # Apply the cut
+        # Cut the cov matrix
+        def cut_matrix(cov, xcov, ellscov, xlim):
+            assert len(cov) == len(xcov) * len(ellscov), 'Input matrix has size {}, different than {} x {}'.format(len(cov), len(xcov), len(ellscov))
+            indices = []
+            for ell, xlim in xlim.items():
+                index = ellscov.index(ell) * len(xcov) + np.arange(len(xcov))
+                index = index[(xcov >= xlim[0]) & (xcov <= xlim[1])]
+                indices.append(index)
+            indices = np.concatenate(indices, axis=0)
+            return cov[np.ix_(indices, indices)]
+        self.cov = cut_matrix(cov_full, kcenters, ellscov, klim) # Shape: (3N, 3N)
+        # Precompute Cholesky decomposition
+        self.cov = 0.5 * (self.cov + self.cov.T)
+        self.cov += 1e-8 * np.eye(len(self.cov))
+        self.cov_chol = cho_factor(self.cov, lower=True)
+        print(">>> PkMultipolesLikelihood initialized")
+
+    def get_requirements(self):
+        return {}
+
+    def logp(self, **params_values):
+        # Build parameter vector in correct order
+        theta = [
+            params_values["h"],
+            params_values["omega_b"],
+            params_values["omega_cdm"],
+            params_values["ns"],
+            params_values["log10As"],
+            params_values["Mnu"],
+            params_values["b1"]
+        ]
+        # Model prediction
+        k_model, P0_model, P2_model, P4_model = compute_multipoles_model(theta, self.zeff, self.kmax)
+        # Build full model vector (should be same length as data_vector)
+        model_vector = np.concatenate([P0_model, P2_model, P4_model])
+        # Compute residuals and chi^2
+        delta = self.data_vector - model_vector
+        chi2 = np.dot(delta, cho_solve(self.cov_chol, delta))
+        return -0.5 * chi2
---
+++ FirstGen/likelihood/get_model.py
@@ -0,0 +1,79 @@
+import numpy as np
+import camb
+from camb import model
+basepath = '/Users/hugor/OneDrive/Escritorio/NISDB/first_gen/data/'
+
+def compute_multipoles_model(CosmoParams, zeff = 0.8, kmax = 0.1):
+    """
+    Compute the power spectrum multipoles (P0, P2, P4) using the NISDB model.
+    """
+    # Cosmological parameters
+    h, omega_b, omega_cdm, ns, log10As, Mnu, b1 = CosmoParams
+    # Getting ready CAMB
+    pars = camb.CAMBparams()
+    pars.set_cosmology(H0=h*100, ombh2=omega_b, omch2=omega_cdm, omk=0.0, 
+                    tau=0.0561, standard_neutrino_neff=3.04, num_massive_neutrinos=1, 
+                    neutrino_hierarchy='normal', mnu=Mnu, nnu=3.04)
+    pars.InitPower.set_params(ns=ns, As=np.exp(log10As)/1e10)
+    pars.set_matter_power(kmax=0.5, redshifts=[zeff, zeff + 0.001])
+    results = camb.get_results(pars)
+    # Get Power spectrum of matter
+    k_full, _, PK = results.get_linear_matter_power_spectrum(hubble_units=True, k_hunit=True)
+    Pk = PK[0,:]
+    # Get Power spectrum of baryons+CDM at the two redshift (for computing the growth rate f_cb(k) later)
+    _, _, PK_cb = results.get_linear_matter_power_spectrum(var1='delta_nonu', var2='delta_nonu', hubble_units=True, k_hunit=True)
+    Pk_cb = PK_cb[0,:]
+    Pk_cb_z2 = PK_cb[1,:]
+    # Get transfer functions of cb and matter
+    trans = results.get_matter_transfer_data()
+    T_m = trans.transfer_data[model.Transfer_tot-1,:,0]
+    T_cb = trans.transfer_data[model.Transfer_nonu-1,:,0]
+    # Computing f_cb(k) around z = zeff
+    a1 = 1.0 / (1.0 + zeff)
+    a2 = 1.0 / (1.0 + zeff + 0.001)
+    dlnP = np.log(Pk_cb_z2) - np.log(Pk_cb)
+    dln_a = np.log(a2) - np.log(a1)
+    f_cb = 0.5 * dlnP / dln_a
+    # Get the bias of galaxies with respect to the total matter
+    bm = b1*(T_cb/T_m)
+    # Get the effective growth rate with respect to the total matter
+    f_eff = f_cb*(T_cb / T_m)
+    # Define mu values
+    mu_vals = np.linspace(-1, 1, len(Pk))
+    # Computing the power spectrum with RSDs 
+    P_k_mu = np.array([bm**2*(1 + f_eff/bm * mu_i**2)**2 * Pk for mu_i in mu_vals])
+    P_k_mu =  P_k_mu.T
+    # mu_vals = np.linspace(-1, 1, P_k_mu.shape[1])  # Ensure match with P_k_mu's shape
+    # Define Legendre polynomials of Legendre
+    def L0(mu):
+        return np.ones_like(mu)
+    def L2(mu):
+        return 0.5 * (3 * mu**2 - 1)
+    def L4(mu):
+        return (35 * mu**4 - 30 * mu**2 + 3) / 8
+    # Compute Legendre polynomials
+    L_ell0 = L0(mu_vals)
+    L_ell2 = L2(mu_vals)
+    L_ell4 = L4(mu_vals)
+    # Multiply each row in P_k_mu by the Legendre polynomials
+    integrand0 = P_k_mu * L_ell0[np.newaxis, :]
+    integrand2 = P_k_mu * L_ell2[np.newaxis, :]
+    integrand4 = P_k_mu * L_ell4[np.newaxis, :]
+    # Integrate over μ using trapezoidal rule along axis=1 (μ-axis)
+    integral0 = np.trapz(integrand0, x=mu_vals, axis=1)
+    integral2 = np.trapz(integrand2, x=mu_vals, axis=1)
+    integral4 = np.trapz(integrand4, x=mu_vals, axis=1)
+    # Multipole prefactors
+    P_ell0 = 0.5 * integral0
+    P_ell2 = 2.5 * integral2
+    P_ell4 = 4.5 * integral4
+    # Interpolate Pk, Pk_cb and transfer funcions in the bining of k
+    k_fn = (basepath + 'k.txt')
+    k_arr = np.loadtxt(k_fn)
+    mask = k_arr <= kmax
+    k_arr = k_arr[mask]
+    P_ell0  = np.interp(k_arr, k_full, P_ell0)
+    P_ell2  = np.interp(k_arr, k_full, P_ell2)
+    P_ell4  = np.interp(k_arr, k_full, P_ell4)
+    # Return multipoles
+    return k_arr, P_ell0, P_ell2, P_ell4
